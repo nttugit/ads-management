fd7b6f31f1fd0ea53bab256fc2cbbb18eefabbcd
Your Name <vinhmai2099@gmail.com>
2024-01-07 20:56:03 +0700

    Update ads-location.test.js file

--- tests/ads-location.test.js
+++ tests/ads-location.test.js
@@ -1,3 +1,13 @@
-console.log('test ads');
+// Tạo một hàm để hiển thị thông báo
+function showMessage() {
+    alert('Xin chào! Bạn đã nhấn vào nút này.');
+}
+
+// Lấy tham chiếu đến nút trong HTML bằng ID
+const buttonElement = document.getElementById('myButton');
+
+// Thêm sự kiện click cho nút
+buttonElement.addEventListener('click', showMessage);
+
 
 export default ads-location.test;
